ef35e269cb3e2ff722a41fad758e6294a1e61fb7
Anna <alexanet855@gmail.com>
2023-04-03 10:07:43 +0300

    Update App.test.tsx

--- src/__tests__/App.test.tsx
+++ src/__tests__/App.test.tsx
@@ -3,7 +3,7 @@ import { render, screen } from '@testing-library/react';
 import '@testing-library/jest-dom';
 import App from '../App';
 
-test('renders learn react link', () => {
+test('find text', () => {
     render(<App />);
     screen.debug();
     const HeroDescription = screen.getByText(/Choose hero/i);
